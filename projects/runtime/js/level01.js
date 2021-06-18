78890d30254abb2b8822dc5d74e3c2cd801ee0ab
AmberLien <amberlien03@gmail.com>
2021-06-18 15:48:33 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,20 +16,41 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "sawblade", "x": 400, "y": groundY },
-                { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "sawblade", "x": 400, "y": 215 },
+                { "type": "sawblade", "x": 1000, "y": groundY },
+                { "type": "sawblade", "x": 1500, "y": 310 },
+                { "type": "knife","x":700,"y": groundY - 20},
+                { "type": "knife","x": 1100, "y": 210},
+                { "type": "knife","x": 1300, "y": 310},
+                { "type": "enemy", "x": 500, "y": groundY-30},
+                { "type": "reward", "x": 900, "y": groundY-10}
             ]
         };
+        for (var i = 0; i < levelData.gameItems.length; i++){
+            var gameItemObject = levelData.gameItems[i];
+            var gameItemObjectType = gameItemObject.type;
+            var gameItemObjectX = gameItemObject.x;
+            var gameItemObjectY = gameItemObject.y;
+            if (gameItemObjectType === "sawblade"){
+                createSawBlade(gameItemObjectX,gameItemObjectY);
+            } else if (gameItemObjectType === "knife"){
+                createKnife(gameItemObjectX,gameItemObjectY);
+            } else if (gameItemObjectType === "enemy"){
+                createEnemy(gameItemObjectX, gameItemObjectY);
+            } else if (gameItemObjectType === "reward"){
+                createReward(gameItemObjectX,gameItemObjectY);
+            }
+        }
+
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
         game.setDebugMode(true);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
-       createSawBlade(400,215);
-       createSawBlade(1000,groundY);
-       createSawBlade(1500,310);
+
+        //obstacles
+
         function createSawBlade(x,y){
             var hitZoneSize = 25;
             var damageFromObstacle = 10;
@@ -43,9 +64,7 @@ var level01 = function (window) {
             obstacleImage.y = -25
             sawBladeHitZone.addChild(obstacleImage);
         }        
-        createKnife(700,groundY - 20);
-        createKnife(1100,210);
-        createKnife(1300,310);
+
         function createKnife(x,y){
             var hitZoneSize = 25;
             var damageFromObstacle = 5;
@@ -60,6 +79,52 @@ var level01 = function (window) {
             knifeImage.x = -30;
             knifeImage.y = -90;
             knifeHitZone.addChild(knifeImage);
+        }
+
+        //ENEMY CODE STARTS HERE
+        function createEnemy(x,y){
+            var enemy = game.createGameItem('enemy',25);
+            var redSquare = draw.rect(50,50,'red');
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare);
+            enemy.x = x;
+            enemy.y = y;
+            game.addGameItem(enemy);
+
+            enemy.velocityX = -1;
+            enemy.rotationalVelocity = 10;
+
+            enemy.onPlayerCollision = function() {
+               console.log('The enemy has hit Halle');
+                game.changeIntegrity(-10);
+                enemy.fadeOut();
+            };
+
+            enemy.onProjectileCollision = function(){
+                console.log('Halle has hit the enemy');
+                game.increaseScore(100);
+                enemy.fadeOut();
+            }
+        }
+        //ENEMY CODE ENDS HERE
+
+        function createReward(x,y){
+            var reward = game.createGameItem('reward',25);
+            var blueSquare = draw.rect(50,50,'blue');
+            blueSquare.x = -25;
+            blueSquare.y = -25;
+            reward.addChild(blueSquare);
+            reward.x = x;
+            reward.y = y;
+            game.addGameItem(reward);
+
+            reward.velocityX = -1;
+
+            reward.onPlayerCollision = function() {
+                game.increaseScore(50);
+                reward.fadeOut();
+            };
         }
         // DO NOT EDIT CODE BELOW HERE
     }
