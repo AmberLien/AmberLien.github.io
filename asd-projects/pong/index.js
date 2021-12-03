b29fba27f8e871e5f3219aacc7b62226fc068ac7
AmberLien <amberlien03@gmail.com>
2021-12-03 00:31:10 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,7 +12,9 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  let paddleOne = FactoryFunction("#paddleOne"); //creates the object containing info abt paddleOne
+  let paddleTwo = FactoryFunction("#paddleTwo"); //creates the object containing info abt paddleTwo
+  let ball = FactoryFunction("#ball");           //creates the object containing info abt the ball
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -42,6 +44,17 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function FactoryFunction(id){
+    const gameInstance = {};              //creates an instance
+    gameInstance.id = id;                 //creates the id property
+    gameInstance.x = $(id).css("left");   //creates the x property
+    gameInstance.y = $(id).css("top");    //creates the y property
+    gameInstance.width = $(id).width();   //creates the width property
+    gameInstance.height = $(id).height(); //creates the height property
+    gameInstance.speedX = 0;              //creates the speedX property
+    gameInstance.speedY = 0;              //creates the speedY property
+    return gameInstance;                  //returns an instance
+  }
   
   function endGame() {
     // stop the interval timer
