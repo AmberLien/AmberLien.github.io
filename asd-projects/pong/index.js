109acec1f796c97a8cb7867bcd8bba36aee27e48
AmberLien <amberlien03@gmail.com>
2021-12-14 23:54:23 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -20,18 +20,11 @@ function runProgram(){
     "down": 40
   }
 
-  // GAME VARIABLES //
-  //ping pong ball
-  let positionBallX = Math.floor(Math.random() * 200 + 100); //sets the ball's x position randomly in the middle of the board
-  let positionBallY = Math.floor(Math.random() * 200 + 100); //sets the ball's y position randomly in the middle of the board
-  let speedBallX;
-  let speedBallY;
-
   // Game Item Objects
   let paddleOne = FactoryFunction("#paddleOne"); //creates the object containing info abt paddleOne
   let paddleTwo = FactoryFunction("#paddleTwo"); //creates the object containing info abt paddleTwo
   let ball = FactoryFunction("#ball");           //creates the object containing info abt the ball
-
+  let board = FactoryFunction("#board");         //creates the object containing info abt the board
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   startBall();
@@ -54,8 +47,11 @@ function runProgram(){
     $(paddleTwo.id).css("top", paddleTwo.y); //updates the position in the paddleTwo object
 
     //ball's movement
-    ball.x = $(ball.id).css("left", positionBallX); //updates the ball's x position
-    ball.y = $(ball.id).css("top", positionBallY); //updates the ball's y position
+    detectBall(); //checks the ball's speed and position
+    ball.x = ball.x + ball.speedX; //updates the ball's x position w/ the x speed
+    ball.y = ball.y + ball.speedY; //updates the ball's y position w/ the y speed
+    $(ball.id).css("left", ball.x); //updates the ball's x position
+    $(ball.id).css("top", ball.y);  //updates the ball's y position
   }
   
   /* 
@@ -64,18 +60,41 @@ function runProgram(){
   function handleKeyDown(event) {
     //keyboard events
     if (event.which === KEY_OBJECT.w){
-      paddleOne.speedY = -5; //moves paddleOne up
-    } else if (event.which === KEY_OBJECT.s){
-      paddleOne.speedY = 5; //moves paddleOne down
-    } 
+      if (paddleOne.y <= 0){
+        paddleOne.speedY = 0; //prevents paddleOne from moving past upper border
+        paddleOne.y = 0; //moves paddle to proper position
+      } else{
+        paddleOne.speedY = -5 //moves paddleOne up
+      }
+    }
+    if (event.which === KEY_OBJECT.s){
+      if (paddleOne.y + paddleOne.height >= board.height){
+        paddleOne.speedY = 0; //prevents paddle from moving past lower border
+        paddleOne.y = board.height - paddleOne.height; //moves paddle to proper position
+      } else{
+        paddleOne.speedY = 5; //moves paddleOne down
+      }
+   } 
+
     if (event.which === KEY_OBJECT.up){
-      paddleTwo.speedY = -5; //moves paddleTwo up
-    } else if (event.which === KEY_OBJECT.down){
-      paddleTwo.speedY = 5; //moves paddleTwo down
+      if (paddleTwo.y <= 0){
+        paddleTwo.speedY = 0; //prevents paddle from moving past lower border
+        paddleTwo.y = 0; //moves paddle to proper position
+      } else{
+        paddleTwo.speedY = -5; //moves paddleTwo up
+      }
+    }
+      if (event.which === KEY_OBJECT.down){
+        if (paddleTwo.y + paddleTwo.height >= board.height){
+          paddleTwo.speedY = 0; //prevents paddle from moving past upper border
+          paddleTwo.y = board.height - paddleTwo.height //moves paddle to proper position
+        } else{
+          paddleTwo.speedY = 5; //moves paddleTwo down
+        }
     }
   }
 
-  function handleKeyUp(event){
+  function handleKeyUp(event){ //!!!!!WORK ON GETTING THIS TO NOT OCCASIONALLY GLITCH
     if(event.which === KEY_OBJECT.w){
       paddleOne.speedY = 0; //stops paddleOne from moving up
     } else if (event.which === KEY_OBJECT.s){
@@ -97,20 +116,39 @@ function runProgram(){
     gameInstance.id = id;                 //creates the id property
     gameInstance.x = parseFloat($(id).css("left"));   //creates the x property
     gameInstance.y = parseFloat($(id).css("top"));    //creates the y property
-    gameInstance.width = $(id).width();   //creates the width property
-    gameInstance.height = $(id).height(); //creates the height property
+    gameInstance.width = parseFloat($(id).width());   //creates the width property
+    gameInstance.height = parseFloat($(id).height()); //creates the height property
     gameInstance.speedX = 0;              //creates the speedX property
     gameInstance.speedY = 0;              //creates the speedY property
     gameInstance.color - $(id).css("background-color");
     return gameInstance;                  //returns an instance
   }
 
-  function startBall(){ //currently being worked on
-    let coinFlip = Math.floor(Math.random() * 10); //randomly generates a number to determine the ball's random speed
-    speedBallX = coinFlip > 5 ? 1 : -1; //determines which direction the ball is moving in the x-direction
-    speedBallY = coinFlip > 5 ? 1 : -1; //determines which direction the ball is moving in the y-direction  
+  function startBall(){
+    ball.x = Math.random() * 200 + 100; //sets the ball's initial x position around the middle
+    ball.y = Math.random() * 200 + 100; //sets the ball's initial y position around the middle
+    ball.speedX = (Math.random() * 3 + 2) * Math.random() > .5 ? 1 : -1; //assigns the x speed b/w -5 and 5
+    ball.speedY = (Math.random() * 3 + 2) * Math.random() > .5 ? 1 : -1; //assigns the y speed b/w -5 and 5  
   }
-  
+
+  function detectBall(){
+    if ((ball.x <= 0) || ((ball.x + ball.width) >= board.width)) { //prevents ball from moving past left and right borders
+      ball.speedX *= -1; //reverses ball's direction
+    }
+    if ((ball.y <= 0) || ((ball.y + ball.height) >= board.height)){ //prevents ball from moving past upper and lower borders
+      ball.speedY *= -1; //reverses ball's direction
+    }
+  }
+  /* MAYBE
+  function detectBorder(movingObject, stationaryObject){
+    if (movingObject.y <= 0){
+      movingObject.y = 0;
+    }
+    if (movingObject.y + stationaryObject.height >= stationaryObject.height){
+      movingObject.y = stationaryObject.height - movingObject.height;
+    }
+  }
+  */
 
   function endGame() {
     // stop the interval timer
